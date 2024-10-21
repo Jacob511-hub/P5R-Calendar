bce463b7b6f9a388dfd83cfd58453981361c168b
Jacob511-hub <jacob511@outlook.com>
2024-10-21 14:31:25 -0230

    Added MUI accordion for Confidants

--- src/components/activities.jsx
+++ src/components/activities.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Divider from '@mui/material/Divider';
 import ConfidantContainer from './confidantContainer';
 import { tarot, tarotNames } from '../hooks/confidantassets';
 
@@ -10,14 +15,40 @@ const Activities = () => {
                 src="src/assets/dagger.png">
             </img>
             <div className="activities-display">
-                <img src="src/assets/confidants.png"></img>
-                {Array.from({ length: tarot.length }).map((_, index) => (
-                    <ConfidantContainer 
-                        key={index}
-                        tarot={tarot[index]}
-                        name={tarotNames[index]}
-                    />
-                ))}
+                <img src="src/assets/activities.png"></img>
+                <Divider variant="middle" 
+                    style={{
+                        borderColor: 'white',
+                    }}
+                />
+                <Accordion
+                    style={{
+                        backgroundColor: 'transparent', // Make the entire accordion transparent
+                        boxShadow: 'none', // Remove box shadow
+                        color: 'white'
+                    }}
+                  >
+                    <AccordionSummary
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                        style={{
+                            backgroundImage: 'url("src/assets/confidants.png")',
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            height: '75px'
+                        }}
+                    >
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        {Array.from({ length: tarot.length }).map((_, index) => (
+                            <ConfidantContainer 
+                                key={index}
+                                tarot={tarot[index]}
+                                name={tarotNames[index]}
+                            />
+                        ))}
+                    </AccordionDetails>
+                </Accordion>
             </div>
         </div>
     )
