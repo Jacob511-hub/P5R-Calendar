49a1b1898f50cbfc7399e8735fbebbd4b2662dc5
Jacob511-hub <jacob511@outlook.com>
2024-10-24 15:14:35 -0230

    Adjusted text styling and formatting for activities and info

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -8,8 +8,10 @@ const Info = ({ dateDisplay, weekdayDisplay, activityDisplay }) => {
                 className="dagger"
                 src="src/assets/dagger.png">
             </img>
-            <h1 id="date-display">{dateDisplay}</h1>
-            <h1 id="weekday-display">{weekdayDisplay}</h1>
+            <div className="info-date">
+                <h1 id="date-display">{dateDisplay}</h1>
+                <h1 id="weekday-display">{weekdayDisplay}</h1>
+            </div>
             {activityDisplay.length > 0 ? (
                 <ul style={{listStyleType: 'none', padding: 0, margin: 0}}>
                 {activityDisplay.map((name, index) => (
