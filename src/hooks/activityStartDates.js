cac20a0e8a46413d6d20740a04c864acb10d198f
Jacob511-hub <jacob511@outlook.com>
2024-10-28 13:53:23 -0230

    Added start dates for all Confidants

--- src/hooks/activityStartDates.js
+++ src/hooks/activityStartDates.js
@@ -1,6 +1,76 @@
+export const PriestessStart = {
+    month: 6,
+    day: 24
+}
+
+export const EmpressStart = {
+    month: 10,
+    day: 30
+}
+
+export const EmperorStart = {
+    month: 6,
+    day: 18
+}
+
+export const HierophantStart = {
+    month: 4,
+    day: 20
+}
+
 export const LoversStart = {
     month: 4,
     day: 15
+}
+
+export const ChariotStart = {
+    month: 4,
+    day: 12
+}
+
+export const JusticeStart = {
+    month: 6,
+    day: 10
+}
+
+export const HermitStart = {
+    month: 8,
+    day: 31
+}
+
+export const FortuneStart = {
+    month: 6,
+    day: 22
+}
+
+export const StrengthStart = {
+    month: 5,
+    day: 18
+}
+
+export const HangedManStart = {
+    month: 5,
+    day: 6
+}
+
+export const DeathStart = {
+    month: 4,
+    day: 18
+}
+
+export const TemperanceStart = {
+    month: 5,
+    day: 25
+}
+
+export const DevilStart = {
+    month: 6,
+    day: 23
+}
+
+export const TowerStart = {
+    month: 9,
+    day: 5
 }
 
 export const StarStart = {
@@ -8,7 +78,45 @@ export const StarStart = {
     day: 25
 }
 
+export const MoonStart = {
+    month: 5,
+    day: 6
+}
+
+export const SunStart = {
+    month: 5,
+    day: 10
+}
+
+export const FaithStart = {
+    month: 5,
+    day: 30
+}
+
+export const CouncillorStart = {
+    month: 5,
+    day: 13
+}
+
 export const activityStartNames = [
+    { name: "Priestess", startDate: PriestessStart },
+    { name: "Empress", startDate: EmpressStart },
+    { name: "Lovers", startDate: EmperorStart },
+    { name: "Hierophant", startDate: HierophantStart },
     { name: "Lovers", startDate: LoversStart },
+    { name: "Chariot", startDate: ChariotStart },
+    { name: "Justice", startDate: JusticeStart },
+    { name: "Hermit", startDate: HermitStart },
+    { name: "Fortune", startDate: FortuneStart },
+    { name: "Strength", startDate: StrengthStart },
+    { name: "Hanged Man", startDate: HangedManStart },
+    { name: "Death", startDate: DeathStart },
+    { name: "Temperance", startDate: TemperanceStart },
+    { name: "Devil", startDate: DevilStart },
+    { name: "Tower", startDate: TowerStart },
     { name: "Star", startDate: StarStart },
+    { name: "Moon", startDate: MoonStart },
+    { name: "Sun", startDate: SunStart },
+    { name: "Faith", startDate: FaithStart },
+    { name: "Councillor", startDate: CouncillorStart },
 ]
