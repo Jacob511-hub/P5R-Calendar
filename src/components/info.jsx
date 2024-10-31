59c142ec05e387c1140b73a61294100078505367
Jacob511-hub <jacob511@outlook.com>
2024-10-31 14:40:05 -0230

    Made info containers into components

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import Divider from '@mui/material/Divider';
+import React from 'react';
+import InfoBox from './infoBox';
 
 const Info = ({ dateDisplay, weekdayDisplay, activityStartDisplay, activityDisplayDay, activityDisplayNight, activityDisplayAuto, activityDisplayDates }) => {
     return (
@@ -14,116 +13,11 @@ const Info = ({ dateDisplay, weekdayDisplay, activityStartDisplay, activityDispl
                     <h1 id="date-display">{dateDisplay}</h1>
                     <h1 id="weekday-display">{weekdayDisplay}</h1>
                 </div>
-                {activityStartDisplay.length > 0 ? (
-                    <>
-                        <h1 id="start-header">Start</h1>
-                        <Divider variant="middle" 
-                            style={{
-                                borderColor: 'white',
-                            }}
-                        />
-                        <ul style={{listStyleType: 'none', padding: 0, margin: 0}}>
-                        {activityStartDisplay.map((name, index) => (
-                            <h2 key={index}>{name}</h2>
-                        ))}
-                        </ul>
-                        <Divider variant="middle" 
-                            style={{
-                                borderColor: 'white',
-                            }}
-                        />
-                    </>
-                ) : (
-                    <p style={{margin: 0}}></p>
-                )}
-                {activityDisplayAuto.length > 0 ? (
-                <>
-                    <h1 id="auto-header">Auto</h1>
-                    <Divider variant="middle" 
-                        style={{
-                            borderColor: 'white',
-                        }}
-                    />
-                    <ul style={{listStyleType: 'none', padding: 0, margin: 0}}>
-                    {activityDisplayAuto.map((name, index) => (
-                        <h2 key={index}>{name}</h2>
-                    ))}
-                    </ul>
-                    <Divider variant="middle" 
-                        style={{
-                            borderColor: 'white',
-                        }}
-                    />
-                </>
-                ) : (
-                    <p style={{margin: 0}}></p>
-                )}
-                {activityDisplayDay.length > 0 ? (
-                <>
-                    <h1 id="day-header">Day</h1>
-                    <Divider variant="middle" 
-                        style={{
-                            borderColor: 'white',
-                        }}
-                    />
-                    <ul style={{listStyleType: 'none', padding: 0, margin: 0}}>
-                    {activityDisplayDay.map((name, index) => (
-                        <h2 key={index}>{name}</h2>
-                    ))}
-                    </ul>
-                    <Divider variant="middle" 
-                        style={{
-                            borderColor: 'white',
-                        }}
-                    />
-                </>
-                ) : (
-                    <p style={{margin: 0}}></p>
-                )}
-                {activityDisplayNight.length > 0 ? (
-                <>
-                    <h1 id="night-header">Night</h1>
-                    <Divider variant="middle" 
-                        style={{
-                            borderColor: 'white',
-                        }}
-                    />
-                    <ul style={{listStyleType: 'none', padding: 0, margin: 0}}>
-                    {activityDisplayNight.map((name, index) => (
-                        <h2 key={index}>{name}</h2>
-                    ))}
-                    </ul>
-                    <Divider variant="middle" 
-                        style={{
-                            borderColor: 'white',
-                        }}
-                    />
-                </>
-                ) : (
-                    <p style={{margin: 0}}></p>
-                )}
-                {activityDisplayDates.length > 0 ? (
-                <>
-                    <h1 id="dates-header">Dates</h1>
-                    <Divider variant="middle" 
-                        style={{
-                            borderColor: 'white',
-                        }}
-                    />
-                    <ul style={{listStyleType: 'none', padding: 0, margin: 0}}>
-                    {activityDisplayDates.map((name, index) => (
-                        <h2 key={index}>{name}</h2>
-                    ))}
-                    </ul>
-                    <Divider variant="middle" 
-                        style={{
-                            borderColor: 'white',
-                        }}
-                    />
-                </>
-                ) : (
-                    <p style={{margin: 0}}></p>
-                )}
+                <InfoBox headerText="Start" activityData={activityStartDisplay} />
+                <InfoBox headerText="Auto" activityData={activityDisplayAuto} />
+                <InfoBox headerText="Day" activityData={activityDisplayDay} />
+                <InfoBox headerText="Night" activityData={activityDisplayNight} />
+                <InfoBox headerText="Dates" activityData={activityDisplayDates} />
             </div>
         </div>
     )
---
+++ src/components/infoBox.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Divider from '@mui/material/Divider';
+
+const InfoBox = ({ headerText, activityData }) => {
+    return (
+        <>
+            {activityData.length > 0 ? (
+                <>
+                    <h1 className='info-header'>{headerText}</h1>
+                    <Divider variant="middle" 
+                        style={{
+                            borderColor: 'white',
+                        }}
+                    />
+                    <ul style={{ listStyleType: 'none', padding: 0, margin: 0 }}>
+                        {activityData.map((name, index) => (
+                            <h2 key={index}>{name}</h2>
+                        ))}
+                    </ul>
+                    <Divider variant="middle" 
+                        style={{
+                            borderColor: 'white',
+                        }}
+                    />
+                </>
+            ) : (
+                <p style={{ margin: 0 }}></p>
+            )}
+        </>
+    );
+};
+
+export default InfoBox;
