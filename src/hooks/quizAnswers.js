f0b7afa944c1e77487fb7f3daf0ad4e121b76121
Jacob511-hub <jacob511@outlook.com>
2024-11-27 15:34:26 -0330

    Began adding TV Quiz Show answers

---
+++ src/hooks/quizAnswers.js
@@ -0,0 +1,15 @@
+class QuizAnswers {
+    constructor(date, answer) {
+        this.date = date;
+        this.answer = answer;
+    }
+}
+
+const TVQuizAnswers = [
+    new QuizAnswers(
+        "5/19",
+        "B: Using noise to cause distress.",
+    ),
+];
+
+export { TVQuizAnswers };
