2818b4e786934b7ed7d52776a0fedde8973b65a7
Jacob511-hub <jacob511@outlook.com>
2024-11-28 11:01:00 -0330

    Added remaining quiz show answers

--- src/hooks/quizAnswers.js
+++ src/hooks/quizAnswers.js
@@ -10,6 +10,46 @@ const TVQuizAnswers = [
         "5/19",
         "B: Using noise to cause distress.",
     ),
+    new QuizAnswers(
+        "5/26",
+        "A: Queen of evidence.",
+    ),
+    new QuizAnswers(
+        "6/2",
+        "B: Probation officers are unpaid.",
+    ),
+    new QuizAnswers(
+        "6/23",
+        "B: Ordering food without money.",
+    ),
+    new QuizAnswers(
+        "8/4",
+        "B: Joyride, but return the car.",
+    ),
+    new QuizAnswers(
+        "9/22",
+        "A: 10 years in prison.",
+    ),
+    new QuizAnswers(
+        "9/29",
+        "A: Divulging someone's crimes.",
+    ),
+    new QuizAnswers(
+        "11/10",
+        "B: You in front of your TV.",
+    ),
+    new QuizAnswers(
+        "11/24",
+        "A: The speaker of the house decides.",
+    ),
+    new QuizAnswers(
+        "12/8",
+        "A: Attempted murder.",
+    ),
+    new QuizAnswers(
+        "12/15",
+        "B: Can't be done by wanted criminals.",
+    ),
 ];
 
 export { TVQuizAnswers };
