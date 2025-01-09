fe8c90af210493b76f4fb73155832a7c2c9ff9a3
Jacob511-hub <jacob511@outlook.com>
2025-01-09 16:10:11 -0330

    Added all remaining exam answers

--- src/hooks/classroom.js
+++ src/hooks/classroom.js
@@ -38,6 +38,56 @@ const ExamAnswers = [
         `The Devil's Dictionary
         \nFemme Fatale`,
     ),
+    new ClassroomQuestion(
+        "7/13",
+        `64 degrees
+        \nZhuge Liang
+        \nBarbarians' heads
+        \nTo offer them instead of heads`,
+    ),
+    new ClassroomQuestion(
+        "7/14",
+        `Red King Crab (Paralithodes)
+        \nIt caused confusion in the economy`,
+    ),
+    new ClassroomQuestion(
+        "7/15",
+        `Raining cats and dogs
+        \nDemon guts`,
+    ),
+    new ClassroomQuestion(
+        "10/17",
+        `32 surfaces
+        \nIt used to be one color
+        \nBlack and white picture`,
+    ),
+    new ClassroomQuestion(
+        "10/18",
+        `Charles-Henri Sanson
+        \nIt's a hereditary profession`,
+    ),
+    new ClassroomQuestion(
+        "10/19",
+        `Slave labor
+        \n3 bees in all of Europe`,
+    ),
+    new ClassroomQuestion(
+        "12/20",
+        `D
+        \nOver one billion yen
+        \nHaving his head displayed
+        \nA performance`,
+    ),
+    new ClassroomQuestion(
+        "12/21",
+        `Hearts
+        \nAttend`,
+    ),
+    new ClassroomQuestion(
+        "12/22",
+        `Japan
+        \nDreadnought`,
+    ),
 ]
 
 export { ClassroomQuestions, ExamAnswers };
