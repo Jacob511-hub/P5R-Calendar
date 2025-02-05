323340bac76e8d2cbe326b0361af5c124b6fb645
Jacob511-hub <jacob511@outlook.com>
2025-02-05 15:32:48 -0330

    Added Maid Café details

--- src/hooks/facilities.js
+++ src/hooks/facilities.js
@@ -19,6 +19,22 @@ const facilities = [
         ],
         ["Charm", "Guts"]
     ),
+    new Facility(
+        "Maid Café",
+        "Any",
+        [
+            "No mistake: Charm +3",
+            "Mistake, forgive maid: Charm +3, Kindness +2",
+            "Mistake, don't forgive maid: Charm +3, Guts +2",
+            "Earn 20 stamps: Charm +2",
+            "Saturdays: Earn +4 stamps",
+            "Relaxing Coffee: ¥1,000, 1 stamp, 50% Mistake",
+            "Oo-hot Tea: ¥2,000, 2 stamp, 60% Mistake",
+            "Love★Pancake: ¥3,000, 3 stamp, 70% Mistake",
+            "Sincere Omelette: ¥5,000, 5 stamp, 90% Mistake",
+        ],
+        ["Charm", "Kindness", "Guts"]
+    ),
 ];
 
 export { facilities }
